8903203ab68c903645e2ee0fd645e31bece13496
Geyang Xie <xiegeyang@gmail.com>
2016-10-14 16:20:48 -0500

    Change ContactHis to Object

---
+++ ContactHis.java
@@ -0,0 +1,17 @@
+package Geyang;
+
+public class ContactHis {
+	private int times;
+
+	public int getTimes() {
+		return times;
+	}
+
+	public void setTimes(int time) {
+		this.times = time;
+	}
+	
+	public ContactHis(int time){
+		this.times = time;
+	}
+}
